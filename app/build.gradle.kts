96891a28f3329b8c28c031b2ec679059c7828870
BarbotteLove <hemondchristopher@gmail.com>
2024-03-13 10:55:35 -0400

    Pie charts
    Figuring it out

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,11 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.4.0")
+
+    //Pie chart implementation and graphs
+    implementation ("com.github.blackfizz:eazegraph:1.2.5l@aar")
+    implementation ("com.nineoldandroids:library:2.4.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
